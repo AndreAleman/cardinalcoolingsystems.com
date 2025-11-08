e6b90339371431c541493eb3a2acc5a03c1cb49a
AndreAleman <davidandrealeman@gmail.com>
2025-11-08 11:19:44 -0700

    gtm

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -10,11 +10,22 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <head>
-        {/* Google tag (gtag.js) */}
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W3H2TDDZ');
+            `,
+          }}
+        />
+        {/* Google Analytics (if also needed) */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-LJL2LPB4T5"
-          dangerouslySetInnerHTML={undefined as any}
         />
         <script
           dangerouslySetInnerHTML={{
@@ -22,12 +33,21 @@ export default function RootLayout(props: { children: React.ReactNode }) {
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-LJJ2LPB4T5');
+              gtag('config', 'G-LJL2LPB4T5');
             `,
           }}
         />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-W3H2TDDZ"
+              height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            `,
+          }}
+        />
         <main className="relative">{props.children}</main>
       </body>
     </html>
