1e5979c3464e972f5924e7377a66017beb131c2d
AndreAleman <davidandrealeman@gmail.com>
2025-12-04 10:58:13 -0500

    ads tag

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -9,44 +9,45 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
-<head>
-  {/* Initialize Data Layer FIRST */}
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        window.dataLayer = window.dataLayer || [];
-      `,
-    }}
-  />
-  
-  {/* Google Tag Manager */}
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-W3H2TDDZ');
-      `,
-    }}
-  />
-  
-  {/* Google Analytics */}
-  <script
-    async
-    src="https://www.googletagmanager.com/gtag/js?id=G-LJL2LPB4T5"
-  />
-  <script
-    dangerouslySetInnerHTML={{
-      __html: `
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', 'G-LJL2LPB4T5');
-      `,
-    }}
-  />
-</head>
+      <head>
+        {/* Initialize Data Layer FIRST */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+            `,
+          }}
+        />
+        
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-W3H2TDDZ');
+            `,
+          }}
+        />
+        
+        {/* Google Analytics - Primary */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-LJL2LPB4T5"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-LJL2LPB4T5');
+              gtag('config', 'G-30XK2WTS1V');
+            `,
+          }}
+        />
+      </head>
 
       <body>
         {/* Google Tag Manager (noscript) */}
