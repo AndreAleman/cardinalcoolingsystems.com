a16aff5cefb3afd9e58965864e0e0742b0aab50e
AndreAleman <davidandrealeman@gmail.com>
2026-05-22 19:51:36 -0600

    feat: add PostHog analytics via lazyOnload script
    Loads the PostHog snippet through next/script with strategy="lazyOnload"
    so it does not contend with hydration on mobile. Renders only when
    NEXT_PUBLIC_POSTHOG_KEY is set; falls back to https://us.i.posthog.com
    unless NEXT_PUBLIC_POSTHOG_HOST overrides. capture_pageview is set to
    'history_change' for App Router SPA navigation tracking, and
    person_profiles to 'identified_only' to keep volume down.

--- storefront/src/app/layout.tsx
+++ storefront/src/app/layout.tsx
@@ -18,6 +18,8 @@ export const metadata: Metadata = {
 
 const GTM_ID = "GTM-W3H2TDDZ"
 const GA_ID = "G-LJL2LPB4T5"
+const POSTHOG_KEY = process.env.NEXT_PUBLIC_POSTHOG_KEY
+const POSTHOG_HOST = process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com"
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -28,6 +30,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <link rel="dns-prefetch" href="https://t.contentsquare.net" />
         <link rel="dns-prefetch" href="https://r2.leadsy.ai" />
         <link rel="dns-prefetch" href="https://bucket-production-02b9.up.railway.app" />
+        {POSTHOG_KEY && <link rel="dns-prefetch" href={POSTHOG_HOST} />}
       </head>
       <body>
         <noscript
@@ -72,6 +75,13 @@ gtag('config', '${GA_ID}');`}
           data-pid="1BqpijfvlFWlLguUn"
           data-version="062024"
         />
+
+        {POSTHOG_KEY && (
+          <Script id="posthog-init" strategy="lazyOnload">
+            {`!function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.crossOrigin="anonymous",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="init me ws ys ps bs capture je Di ks register register_once register_for_session unregister unregister_for_session Ps getFeatureFlag getFeatureFlagPayload isFeatureEnabled reloadFeatureFlags updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures on onFeatureFlags onSurveysLoaded onSessionId getSurveys getActiveMatchingSurveys renderSurvey canRenderSurvey canRenderSurveyAsync identify setPersonProperties group resetGroups setPersonPropertiesForFlags resetPersonPropertiesForFlags setGroupPropertiesForFlags resetGroupPropertiesForFlags reset get_distinct_id getGroups get_session_id get_session_replay_url alias set_config startSessionRecording stopSessionRecording sessionRecordingStarted captureException loadToolbar get_property getSessionProperty Es \$s createPersonProfile Is opt_in_capturing opt_out_capturing has_opted_in_capturing has_opted_out_capturing clear_opt_in_out_capturing Ss debug I As getPageViewId captureTraceFeedback captureTraceMetric".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+posthog.init('${POSTHOG_KEY}',{api_host:'${POSTHOG_HOST}',capture_pageview:'history_change',person_profiles:'identified_only'});`}
+          </Script>
+        )}
       </body>
     </html>
   )
